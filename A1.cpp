e7aeb8323653b3104eb31756da01645788b24b86
Mansi Lokhande <96643723+mansylokhande01@users.noreply.github.com>
2024-02-01 23:30:16 +0530

    Update A1.cpp

--- A1.cpp
+++ A1.cpp
@@ -1,3 +1,8 @@
+//Implement depth first search algorithm and Breadth First Search algorithm, use an undirected
+// graph and develop a recursive algorithm for searching all the vertices of a graph or tree data
+// structure.
+
+
 #include<bits/stdc++.h>
 using namespace std;
 void bfs_rec(vector<int>vec[],vector<int> &visit, queue<int> & q,vector<int> & ans)
